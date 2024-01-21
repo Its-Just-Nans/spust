7c70b655c9d3ab9e665a3824219240e21fc0f1bf
n4n5 <its.just.n4n5@gmail.com>
2024-01-21 15:29:15 +0100

    feat: add some code

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use axum::extract::{ConnectInfo, Multipart, Request};
+use axum::http::header::{self, CONTENT_TYPE};
+use axum::http::HeaderMap;
+use axum::{
+    http::header::SET_COOKIE,
+    response::{Html, IntoResponse, Redirect},
+    Json,
+};
+use std::net::SocketAddr;
+
+pub async fn api_handler(
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    req: Request,
+) -> impl IntoResponse {
+    println!("------------------------------------");
+    println!("Address: {:?}", addr);
+    println!("Request Method: {}", req.method());
+    println!("Request URI: {}", req.uri());
+    for (key, value) in req.headers() {
+        println!("{}: {}", key, value.to_str().unwrap());
+    }
+    let mut stre = String::new();
+    if let Some(cookie_header) = req.headers().get("Cookie") {
+        let formatted = format!("Cookies: {}", cookie_header.to_str().unwrap());
+        stre.push_str(&formatted);
+    }
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        CONTENT_TYPE,
+        header::HeaderValue::from_static("text/html; charset=utf-8"),
+    );
+    let content = match req.uri().path() {
+        "/api/route1" => {
+            headers.insert(
+                SET_COOKIE,
+                header::HeaderValue::from_static("my_cookie=my_value"),
+            );
+            Html(format!(
+                "/api {addr} Hello from Route 1!<br/><a href='/api'>Home</a> <br/>{stre}"
+            ))
+        }
+        "/api/route2" => Html(format!(
+            "/api {addr} Hello from Route 2!<br/><a href='/api'>Home</a> <br/>{stre}"
+        )),
+        "/api" => Html(format!(
+            "/api {addr}<br/><a href='/api/route1'>route1</a><br/><a href='/api/route2'>route2</a><br/>{stre}"
+        )),
+        _ => {
+            return Redirect::permanent("/api").into_response();
+        }
+    };
+    (headers, content).into_response()
+}
+
+pub async fn upload_handler(mut multipart: Multipart) -> impl IntoResponse {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        println!("Length of `{}` is {} bytes", name, data.len());
+    }
+    let headers = HeaderMap::new();
+    let content = Json("{}");
+    (headers, content).into_response()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::DefaultBodyLimit,
+    routing::{get, post},
+    Router,
+};
+use spust::{api_handler, upload_handler};
+use std::net::SocketAddr;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    const DIST: &str = "dist";
+    let serve_dir =
+        ServeDir::new(DIST).not_found_service(ServeFile::new(format!("{DIST}/index.html")));
+    const FILES: &str = "files";
+    let files_dir =
+        ServeDir::new(FILES).not_found_service(ServeFile::new(format!("{FILES}/index.html")));
+
+    let app = Router::new()
+        .route("/api", get(api_handler))
+        .route("/api/upload", post(upload_handler))
+        .layer(DefaultBodyLimit::max(5 * 1024))
+        .route("/api/*path", get(api_handler))
+        .nest_service(&format!("/{FILES}"), files_dir)
+        .fallback_service(serve_dir);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await;
+    if let Err(e) = listener {
+        println!("Error: {} {}", e, addr);
+        return;
+    }
+    let listener = listener.unwrap();
+    println!("Listening on http://{}", addr);
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
